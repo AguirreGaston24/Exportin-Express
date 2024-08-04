7cba3a9a7a15fc79b818457ef34aa10745ca0dbc
OPNUX <104266497+OPNUX@users.noreply.github.com>
2024-08-04 08:58:34 -0300

    primer push

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosApi = axios.create({
 
     //baseURL: process.env.API_URL, 
-    baseURL: process.env.API_URL,
+    baseURL: 'https://nicolas-landing-back.vercel.app',
     withCredentials: true,
 
 })
